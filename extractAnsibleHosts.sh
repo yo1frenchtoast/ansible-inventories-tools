f66828ffd67ee70dc08c16da914d2c184bc40c4f
Yoan Tanguy <ytanguy.lz@gmail.com>
2018-08-28 17:17:21 +0200

    Handle sections with children

--- extractAnsibleHosts.sh
+++ extractAnsibleHosts.sh
@@ -3,80 +3,85 @@
 ### extract ansible_host list from /etc/ansible/hosts and convert it to bash alias, ytanguy 2017-05-31
 # 2017-11-17 : change to inventories/host_vars ansible format
 
+proceed=false
 while read line
 do
-    unset user
-    unset ip
-    unset port
+    [[ $line =~ "HOSTS definition" ]] && proceed=true
 
-    if [[ ! $line =~ :children && $line =~ ^\[(.*)\] ]]
+    if $proceed
     then
-        group=${BASH_REMATCH[1]}
-    fi
+        unset user
+        unset ip
+        unset port
 
-    if [[ $line =~ ^([a-zA-Z0-9]+.*) ]]
-    then
-	    host=${BASH_REMATCH[1]}
-
-        if [[ -e /etc/ansible/inventories/group_vars/$group ]]
+        if [[ ! $line =~ :children && $line =~ ^\[(.*)\] ]]
         then
-            while read param
-            do
-                if [[ $param =~ ansible_user:\ ([a-z]+) ]]
-                then
-                    user=${BASH_REMATCH[1]}
-                fi
-
-                if [[ $param =~ ansible_port:\ ([0-9]{1,5}) ]]
-                then
-                    port=${BASH_REMATCH[1]}
-                fi
-            done < /etc/ansible/inventories/group_vars/$group
+            group=${BASH_REMATCH[1]}
         fi
 
-        if [[ -e /etc/ansible/inventories/host_vars/$host ]]
+        if [[ $line =~ ^([a-zA-Z0-9]+.*) ]]
         then
-            while read param
-            do
-                if [[ $param =~ ansible_host:\ (([0-9]{1,3}\.?){4}) ]]
-                then
-	                ip=${BASH_REMATCH[1]}
-                fi
+    	    host=${BASH_REMATCH[1]}
 
-                if [[ ! $user || $user == "root" ]]
-                then
-	                if [[ $param =~ ansible_user:\ ([a-z]+) ]]
-                    then
-	                    user=${BASH_REMATCH[1]}
-	                else
-	                    user='root'
-	                fi
-                fi
-
-                if [[ ! $port || $port -eq 22 ]]
-                then
-                    if [[ $param =~ ansible_port:\ ([0-9]{1,5}) ]]
-	                then
-	                    port=${BASH_REMATCH[1]}
-	                else
-	                    port=22
-	                fi
-                fi
-            done < /etc/ansible/inventories/host_vars/$host
-
-            if [[ $1 == "alias" ]]
+            if [[ -e /etc/ansible/inventories/group_vars/$group ]]
             then
-                echo "alias $host='ssh $user@$ip -p $port'"
-            else
-                CONFIG=$(cat <<EOF
+                while read param
+                do
+                    if [[ $param =~ ansible_user:\ ([a-z]+) ]]
+                    then
+                        user=${BASH_REMATCH[1]}
+                    fi
+
+                    if [[ $param =~ ansible_port:\ ([0-9]{1,5}) ]]
+                    then
+                        port=${BASH_REMATCH[1]}
+                    fi
+                done < /etc/ansible/inventories/group_vars/$group
+            fi
+
+            if [[ -e /etc/ansible/inventories/host_vars/$host ]]
+            then
+                while read param
+                do
+                    if [[ $param =~ ansible_host:\ (([0-9]{1,3}\.?){4}) ]]
+                    then
+    	                ip=${BASH_REMATCH[1]}
+                    fi
+
+                    if [[ ! $user || $user == "root" ]]
+                    then
+    	                if [[ $param =~ ansible_user:\ ([a-z]+) ]]
+                        then
+    	                    user=${BASH_REMATCH[1]}
+    	                else
+    	                    user='root'
+    	                fi
+                    fi
+
+                    if [[ ! $port || $port -eq 22 ]]
+                    then
+                        if [[ $param =~ ansible_port:\ ([0-9]{1,5}) ]]
+    	                then
+    	                    port=${BASH_REMATCH[1]}
+    	                else
+    	                    port=22
+    	                fi
+                    fi
+                done < /etc/ansible/inventories/host_vars/$host
+
+                if [[ $1 == "alias" ]]
+                then
+                    echo "alias $host='ssh $user@$ip -p $port'"
+                else
+                    CONFIG=$(cat <<EOF
 Host $host
     User $user
     HostName $ip
     Port $port
-
 EOF
-                )
-                echo "$CONFIG"
+                    )
+                    echo "$CONFIG"
+                fi
             fi
         fi
     fi
